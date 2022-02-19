e6fd23dd1e3ad83d0b8f4867505bbf983eb0de81
Frederico Benevides <fredbene@gmail.com>
2022-02-19 01:48:35 -0300

    Big refactoring to config heroes using vector

--- src/bombcrypto/heroes.clj
+++ src/bombcrypto/heroes.clj
@@ -9,176 +9,205 @@
 (defn- cycle->iteration [cycle iteration]
   (mod cycle iteration))
 
+(def heroes-1-start [:open-heroes
+                     :run-all
+                     :close-heroes])
+(def heroes-1-iteration-1 [:open-heroes
+                           :run-all
+                           :next-page
+                           :next-page
+                           {:go-rest 4}
+                           :close-heroes])
+(def heroes-1-is-time-to-rest [:open-heroes
+                               :rest-all
+                               :close-heroes])
+
+(def heroes-2-start [:open-heroes
+                     :run-all
+                     :close-heroes])
+(def heroes-2-iteration-1 [:open-heroes
+                           :run-all
+                           {:go-home 1}
+                           :next-page
+                           :next-page
+                           {:go-home 4}
+                           :close-heroes])
+(def heroes-2-is-time-to-rest [:open-heroes
+                               :rest-all
+                               :close-heroes])
+
+(def heroes-3-start [:open-heroes
+                     :run-all
+                     :close-heroes])
+(def heroes-3-iteration-1 [:open-heroes
+                           :run-all
+                           :next-page
+                           {:go-rest 2}
+                           {:go-rest 5}
+                           :close-heroes])
+(def heroes-3-is-time-to-rest [:open-heroes
+                               :rest-all
+                               :close-heroes])
+
+(def heroes-4-start [:open-heroes
+                     :run-all
+                     :close-heroes])
+(def heroes-4-iteration-1 [:open-heroes
+                           :run-all
+                           {:go-rest 3}
+                           {:go-rest 4}
+                           :next-page
+                           {:go-rest 2}
+                           {:go-rest 5}
+                           {:go-rest 5}
+                           :close-heroes])
+(def heroes-4-is-time-to-rest [:open-heroes
+                               :rest-all
+                               :close-heroes])
+
+(def heroes-5-start [:open-heroes
+                     :run-all
+                     :close-heroes])
+(def heroes-5-iteration-1 [:open-heroes
+                           :run-all
+                           :next-page
+                           {:go-rest 4}
+                           :close-heroes])
+(def heroes-5-is-time-to-rest [:open-heroes
+                               :rest-all
+                               :close-heroes])
+
+(def heroes-6-start [:open-heroes
+                     :run-all
+                     :close-heroes])
+(def heroes-6-iteration-1 [:open-heroes
+                           :run-all
+                           {:go-rest 1}
+                           {:go-rest 5}
+                           :next-page
+                           :next-page
+                           {:go-rest 3}])
+(def heroes-6-is-time-to-rest [:open-heroes
+                               :rest-all
+                               :close-heroes])
+
+(defn run-heroes [heroes-data browser-id hero-id]
+  (println hero-id " - running heroes on the browser with id:" browser-id)
+  (doseq [d heroes-data]
+    (println "==> Executing:" d)
+    (if (map? d)
+      (let [key (first (keys d))]
+        (cond
+          (= key :go-home) (screen/hero-go-home browser-id (:go-home d))
+          (= key :go-rest) (screen/hero-go-rest browser-id (:go-rest d))))
+      (cond
+        (= d :open-heroes) (screen/open-heroes-popup browser-id)
+        (= d :close-heroes) (screen/close-heroes-popup browser-id)
+        (= d :run-all) (screen/run-all-heroes browser-id)
+        (= d :rest-all) (screen/rest-all-heroes browser-id)
+        (= d :next-page) (do (screen/move-to-hero browser-id 1)
+                             (screen/next-page-of-heroes))))))
+
 (defn all-heroes-go-menu-treasure-hunt []
   (println "All heroes need to go to the menu and go back to the treasure hunt.")
   (doseq [id browser-ids]
     (screen/go-main-menu id)
     (screen/go-treasure-hunt id)))
 
-(defn open-popup-and-run-heroes [hero-id browser-id]
-  (println hero-id "- Starting all heroes for the browser of id" browser-id)
-
-  (screen/open-heroes-popup browser-id)
-  (screen/run-all-heroes browser-id)
-  (screen/close-heroes-popup browser-id))
-
-(defn open-popup-and-rest-heroes [hero-id browser-id]
-  (println hero-id "- Resting all heroes for the browser of id" browser-id)
-  (screen/open-heroes-popup browser-id)
-  (screen/rest-all-heroes browser-id)
-  (screen/close-heroes-popup browser-id))
-
-(defn heroes1-start [_]
-  (let [browser-id (nth browser-ids 0)]
-    (open-popup-and-run-heroes 1 browser-id)))
+(defn heroes1-start [cycle]
+  (let [browser-id (nth browser-ids 0)
+        hero-id 1
+        total-iterations 2
+        iteration (cycle->iteration cycle total-iterations)]
+    (cond
+      (= iteration 0) (run-heroes heroes-1-start browser-id hero-id)
+      (= iteration 1) (run-heroes heroes-1-iteration-1 browser-id hero-id))))
 
 (defn heroes1-is-time-to-rest? [n-time]
   (let [browser-id (nth browser-ids 0)]
     (when (= n-time n-time-to-stop)
       (println "1 - Time to rest/go home" n-time)
-      (open-popup-and-rest-heroes 1 browser-id))))
-
-(defn heroes2-start-iteration-1 [browser-id start]
-  (println "2 - Starting some heroes and making others to rest")
-  (screen/open-heroes-popup browser-id)
-  (screen/run-all-heroes browser-id)
-  (screen/hero-go-home browser-id 1)
-  (screen/hero-go-home browser-id 2)
-  (screen/hero-go-home browser-id 2)
-  (screen/next-page-of-heroes)
-  (screen/next-page-of-heroes)
-  (screen/hero-go-home browser-id 2)
-
-  (when (not start)
-    (screen/rest-all-heroes browser-id))
-
-  (screen/close-heroes-popup browser-id))
+      (run-heroes heroes-1-is-time-to-rest browser-id 1))))
 
 (defn heroes2-start [cycle]
   (let [browser-id (nth browser-ids 1)
+        hero-id 2
         total-iterations 2
         iteration (cycle->iteration cycle total-iterations)]
     (cond
-      (= iteration 0) (open-popup-and-run-heroes 2 browser-id)
-      (= iteration 1) (heroes2-start-iteration-1 browser-id true))))
+      (= iteration 0) (run-heroes heroes-2-start browser-id hero-id)
+      (= iteration 1) (run-heroes heroes-2-iteration-1 browser-id hero-id))))
 
 (defn heroes2-is-time-to-rest? [n-time]
-  (let [browser-id (nth browser-ids 1)]
+  (let [browser-id (nth browser-ids 1)
+        hero-id 2]
     (when (= n-time n-time-to-stop)
       (println "2 - Time to rest/go home" n-time)
-      (heroes2-start-iteration-1 browser-id false))))
-
-(defn heroes3-start-iteration-1 [id start]
-  (println "3 - Starting some heroes and making others to rest")
-  (screen/open-heroes-popup id)
-  (screen/run-all-heroes id)
-  (screen/next-page-of-heroes)
-  (screen/hero-go-rest id 2)
-  (screen/hero-go-rest id 5)
-
-  (when (not start)
-    (screen/rest-all-heroes id))
-
-  (screen/close-heroes-popup id))
+      (run-heroes heroes-2-is-time-to-rest browser-id hero-id))))
 
 (defn heroes3-start [cycle]
   (let [browser-id (nth browser-ids 2)
+        hero-id 3
         total-iterations 2
         iteration (cycle->iteration cycle total-iterations)]
     (cond
-      (= iteration 0) (open-popup-and-run-heroes 3 browser-id)
-      (= iteration 1) (heroes3-start-iteration-1 browser-id true))))
+      (= iteration 0) (run-heroes heroes-3-start browser-id hero-id)
+      (= iteration 1) (run-heroes heroes-3-iteration-1 browser-id hero-id))))
 
 (defn heroes3-is-time-to-rest? [n-time]
-  (let [browser-id (nth browser-ids 2)]
+  (let [browser-id (nth browser-ids 2)
+        hero-id 3]
     (when (= n-time n-time-to-stop)
       (println "3 - Time to rest/go home" n-time)
-      (heroes3-start-iteration-1 browser-id false))))
-
-(defn heroes4-start-iteration-1 [id start]
-  (println "4 - Starting some heroes and making others to rest")
-  (screen/open-heroes-popup id)
-  (screen/run-all-heroes id)
-  (screen/hero-go-rest id 3)
-  (screen/hero-go-rest id 4)
-  (screen/next-page-of-heroes)
-  (screen/hero-go-rest id 2)
-  (screen/hero-go-rest id 5)
-    (screen/hero-go-rest id 5)
-
-  (when (not start)
-    (screen/rest-all-heroes id))
-
-  (screen/close-heroes-popup id))
+      (run-heroes heroes-3-is-time-to-rest browser-id hero-id))))
 
 (defn heroes4-start [cycle]
   (let [browser-id (nth browser-ids 3)
+        hero-id 4
         total-iterations 2
         iteration (cycle->iteration cycle total-iterations)]
     (cond
-      (= iteration 0) (open-popup-and-run-heroes 4 browser-id)
-      (= iteration 1) (heroes4-start-iteration-1 browser-id true))))
+      (= iteration 0) (run-heroes heroes-4-start browser-id hero-id)
+      (= iteration 1) (run-heroes heroes-4-iteration-1 browser-id hero-id))))
 
 (defn heroes4-is-time-to-rest? [n-time]
-  (let [browser-id (nth browser-ids 3)]
+  (let [browser-id (nth browser-ids 3)
+        hero-id 4]
     (when (= n-time n-time-to-stop)
       (println "4 - Time to rest/go home" n-time)
-      (heroes4-start-iteration-1 browser-id false))))
-
-(defn heroes5-start-iteration-1 [id start]
-  (println "5 - Starting some heroes and making others to rest")
-  (screen/open-heroes-popup id)
-  (screen/run-all-heroes id)
-  (screen/next-page-of-heroes)
-  (screen/hero-go-rest id 4)
-
-  (when (not start)
-    (screen/rest-all-heroes id))
-
-  (screen/close-heroes-popup id))
+      (run-heroes heroes-4-is-time-to-rest browser-id hero-id))))
 
 (defn heroes5-start [cycle]
   (let [browser-id (nth browser-ids 4)
+        hero-id 5
         total-iterations 2
         iteration (cycle->iteration cycle total-iterations)]
     (cond
-      (= iteration 0) (open-popup-and-run-heroes 5 browser-id)
-      (= iteration 1) (heroes5-start-iteration-1 browser-id true))))
+      (= iteration 0) (run-heroes heroes-5-start browser-id hero-id)
+      (= iteration 1) (run-heroes heroes-5-iteration-1 browser-id hero-id))))
 
 (defn heroes5-is-time-to-rest? [n-time]
-  (let [browser-id (nth browser-ids 4)]
+  (let [browser-id (nth browser-ids 4)
+        hero-id 5]
     (when (= n-time n-time-to-stop)
       (println "5 - Time to rest/go home" n-time)
-      (heroes5-start-iteration-1 browser-id false))))
-
-(defn heroes6-start-iteration-1 [id start]
-  (println "6 - Starting some heroes and making others to rest")
-  (screen/open-heroes-popup id)
-  (screen/run-all-heroes id)
-  (screen/hero-go-rest id 1)
-  (screen/hero-go-rest id 5)
-  (screen/next-page-of-heroes)
-  (screen/next-page-of-heroes)
-  (screen/hero-go-rest id 3)
-
-  (when (not start)
-    (screen/rest-all-heroes id))
-
-  (screen/close-heroes-popup id))
+      (run-heroes heroes-5-is-time-to-rest browser-id hero-id))))
 
 (defn heroes6-start [cycle]
   (let [browser-id (nth browser-ids 5)
+        hero-id 6
         total-iterations 2
         iteration (cycle->iteration cycle total-iterations)]
     (cond
-      (= iteration 0) (open-popup-and-run-heroes 6 browser-id)
-      (= iteration 1) (heroes3-start-iteration-1 browser-id true))))
+      (= iteration 0) (run-heroes heroes-6-start browser-id hero-id)
+      (= iteration 1) (run-heroes heroes-6-iteration-1 browser-id hero-id))))
 
 (defn heroes6-is-time-to-rest? [n-time]
-  (let [browser-id (nth browser-ids 5)]
+  (let [browser-id (nth browser-ids 5)
+        hero-id 6]
     (when (= n-time n-time-to-stop)
       (println "6 - Time to rest/go home" n-time)
-      (heroes6-start-iteration-1 browser-id false))))
+      (run-heroes heroes-6-is-time-to-rest browser-id hero-id))))
 
 (defn start-all-heroes [cycle]
   (heroes1-start cycle)
