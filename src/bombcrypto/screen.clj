226b6a82e0c7ce8adf25fea13b4650cefa96d616
Frederico Benevides <fredbene@gmail.com>
2022-03-10 16:57:57 -0300

    changed the sleep time

--- src/bombcrypto/screen.clj
+++ src/bombcrypto/screen.clj
@@ -15,7 +15,7 @@
     (mouse/click))
 
   ;; wait popup stay open before going forward
-  (Thread/sleep 1000))
+  (Thread/sleep 3000))
 
 (defn close-heroes-popup [browser-id]
   ;; move to close popup
@@ -26,11 +26,11 @@
 
 (defn run-all-heroes [browser-id]
   (mouse/move-to-and-click browser-id 204 180)
-  (Thread/sleep 3000))
+  (Thread/sleep 2000))
 
 (defn rest-all-heroes [browser-id]
   (mouse/move-to-and-click browser-id 235 180)
-  (Thread/sleep 3000))
+  (Thread/sleep 2000))
 
 (defn move-to-hero [browser-id number]
   (let [index (- number 1)
