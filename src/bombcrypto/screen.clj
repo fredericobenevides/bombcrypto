b1523f21ce8ac12d8f096af4f81bb5ee6432970d
Frederico Benevides <fredbene@gmail.com>
2022-03-20 11:50:53 -0300

    Updated screen config

--- src/bombcrypto/screen.clj
+++ src/bombcrypto/screen.clj
@@ -2,14 +2,14 @@
   (:require [bombcrypto.mouse :as mouse]))
 
 (defn go-main-menu [browser-id]
-  (mouse/move-to-and-click browser-id 30 115)
+  (mouse/move-to-and-click browser-id 12 85)
   (Thread/sleep 1000))
 
 (defn go-treasure-hunt [browser-id]
-  (mouse/move-to-and-click browser-id 250 260))
+  (mouse/move-to-and-click browser-id 235 230))
 
 (defn open-heroes-popup [browser-id]
-  (mouse/move-to browser-id 250 390)
+  (mouse/move-to browser-id 235 360)
 
   (dotimes [_ 2]
     (mouse/click))
@@ -19,42 +19,41 @@
 
 (defn close-heroes-popup [browser-id]
   ;; move to close popup
-  (mouse/move-to-and-click browser-id 274 155)
+  (mouse/move-to-and-click browser-id 263 128)
 
   ;; click in the middle of the screen
-  (mouse/move-to-and-click browser-id 250 260))
+  (mouse/move-to-and-click browser-id 235 230))
 
 (defn run-all-heroes [browser-id]
-  (mouse/move-to-and-click browser-id 204 180)
+  (mouse/move-to-and-click browser-id 190 151)
   (Thread/sleep 2000))
 
 (defn rest-all-heroes [browser-id]
-  (mouse/move-to-and-click browser-id 235 180)
+  (mouse/move-to-and-click browser-id 222 151)
   (Thread/sleep 2000))
 
 (defn move-to-hero [browser-id number]
   (let [index (- number 1)
-        y (-> (* 36 index) (+ 205))]
+        y (-> (* 36 index) (+ 173))]
     (mouse/move-to browser-id 150 y)
-    (mouse/click)
-    ))
+    (mouse/click)))
 
 (defn hero-go-work [browser-id number]
   (let [index (- number 1)
-        y (-> (* 36 index) (+ 205))]
-    (mouse/move-to browser-id 204 y)
+        y (-> (* 36 index) (+ 173))]
+    (mouse/move-to browser-id 190 y)
     (mouse/click)))
 
 (defn hero-go-rest [browser-id number]
   (let [index (- number 1)
-        y (-> (* 36 index) (+ 205))]
-    (mouse/move-to browser-id 235 y)
+        y (-> (* 36 index) (+ 173))]
+    (mouse/move-to browser-id 222 y)
     (mouse/click)))
 
 (defn hero-go-home [browser-id number]
   (let [index (- number 1)
-        y (-> (* 36 index) (+ 205))]
-    (mouse/move-to browser-id 265 y)
+        y (-> (* 36 index) (+ 173))]
+    (mouse/move-to browser-id 254 y)
     (mouse/click)))
 
 (defn previous-page []
@@ -66,10 +65,10 @@
     (mouse/scroll-down)))
 
 (defn open-chest [browser-id]
-  (mouse/move-to-and-click browser-id 430 115)
+  (mouse/move-to-and-click browser-id 420 85)
   (Thread/sleep 1000))
 
 (defn close-chest [browser-id]
-  (mouse/move-to-and-click browser-id 380 158)
+  (mouse/move-to-and-click browser-id 367 128)
   (Thread/sleep 1000))
 
